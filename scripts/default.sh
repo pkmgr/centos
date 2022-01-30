59c1025f9a4f852ced5084bbfe50abaeef4f8dc3
Jason <git-admin@casjaysdev.com>
2022-01-29 23:49:43 -0500

    ✔️🐛 FIXED: scripts/default.sh 🐛✔️
    git update scripts command

--- scripts/default.sh
+++ scripts/default.sh
@@ -163,7 +163,7 @@ if [ -f /etc/casjaysdev/updates/versions/default.txt ]; then
 fi
 if ! builtin type -P systemmgr &>/dev/null; then
   if [[ -d "/usr/local/share/CasjaysDev/scripts" ]]; then
-    run_external "git -C https://github.com/casjay-dotfiles/scripts pull"
+    run_external "git -C /usr/local/share/CasjaysDev/scripts pull"
   else
     run_external "git clone https://github.com/casjay-dotfiles/scripts /usr/local/share/CasjaysDev/scripts"
   fi
@@ -902,7 +902,8 @@ if devnull postmap /etc/postfix/transport /etc/postfix/canonical /etc/postfix/vi
   newaliases &>/dev/null || newaliases.postfix -I &>/dev/null
 fi
 
-run_post "dfmgr install bash misc"
+run_post "dfmgr install misc"
+run_post "dfmgr install bash"
 ##################################################################################################################
 printf_head "Disabling services"
 ##################################################################################################################
